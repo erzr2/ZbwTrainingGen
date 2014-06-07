f4dddf021020ed4332ed4f44d163fef51c32fba8
Erik Partridge <amliterature2012.13@gmail.com>
2014-06-07 18:07:52 -0400

    Loader

--- src/com/erikpartridge/Loader.java
+++ src/com/erikpartridge/Loader.java
@@ -5,6 +5,13 @@ This source code & its compiled program are released under the Apache 2.0 licens
 package com.erikpartridge;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 
@@ -12,8 +19,32 @@ public class Loader {
     
     public static File config;
     
+    public static HashMap<String, URL> resources = new HashMap<>();
     
     public static void load(){
+        configure();
+    }
+    
+    public static void configure(){
+        Scanner in = null;
+        try {
+            in = new Scanner(config);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Loader.class.getName()).log(Level.SEVERE, null, ex);
+        }
         
+        while(in.hasNextLine()){
+            String temp = in.nextLine();
+            
+            String[] dat = temp.split(";");
+                        
+            URL url = null;
+            try {
+                url = new URL(dat[1]);
+            } catch (MalformedURLException ex) {
+                Logger.getLogger(Loader.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            resources.put(dat[0], url);
+        }
     }
 }
