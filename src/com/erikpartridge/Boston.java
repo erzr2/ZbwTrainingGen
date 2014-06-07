4ef6ee77fed6272b532bca7e04fa536062f18573
Erik Partridge <amliterature2012.13@gmail.com>
2014-06-07 12:51:24 -0400

    Edited Boston

--- src/com/erikpartridge/Boston.java
+++ src/com/erikpartridge/Boston.java
@@ -1,5 +1,5 @@
-/** (c) Erik Malmstrom-Partridge 2014
-This code is provided as-is without any warranty
+/*
+*This code is provided as-is without any warranty
 * This code and its compiled program are released under the Apache 2.0 license
 *
 **/
@@ -9,5 +9,15 @@ package com.erikpartridge;
 
 public class Boston {
 
+    private final Random rand = new Random();
     
+    //Makes an aircraft, false if failed (ie. no parking spaces)
+    //Difficulty between 0-10, 5 = average 1 error per flight plan, 10 = 2, etc.
+    public static boolean makePlane(double difficulty, Airport airport, File perfect){
+        
+    }
+    
+    private Airplane loadPerfect(File f){
+        
+    }
 }
